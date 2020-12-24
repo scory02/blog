33f4440216bda51b512e11499bb3cfe64c9f2b93
scory02 <2982879946@qq.com>
2020-12-24 21:30:58 +0800

    Site updated: 2020-12-24 21:30:57

--- js/src/utils.js
+++ js/src/utils.js
@@ -10,7 +10,6 @@ NexT.utils = NexT.$u = {
       .not('.group-picture img, .post-gallery img')
       .each(function () {
         var $image = $(this);
-				if ($(this).hasClass('nofancybox')) return;
         var imageTitle = $image.attr('title');
         var $imageWrapLink = $image.parent('a');
 
